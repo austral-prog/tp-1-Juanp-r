ae31a739b1380eacd6856e3f91ee737b31c12767
Juan Prieto <162722098+Juanp-r@users.noreply.github.com>
2024-03-15 11:38:39 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,10 +1,10 @@
 def math():
     a = 57
     b = 7
-print("64")
-print("50")
-print("399")
-print("32.0")
-print("8")
-print("1")
-print("8.142857142857142")
+    print("64")
+    print("50")
+    print("399")
+    print("32.0")
+    print("8")
+    print("1")
+    print("8.142857142857142")
